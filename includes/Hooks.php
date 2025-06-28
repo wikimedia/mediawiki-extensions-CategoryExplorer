dec2bef8b7cb334476b435d6af803b85d0dcd304
Fomafix <fomafix@googlemail.com>
2025-06-28 22:35:37 +0000

    Inject services into Hooks
    Change-Id: I898063d46e8cce80c2331e4ade730cf1de913092

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -1,32 +1,39 @@
 <?php
 namespace CategoryExplorer;
 
-use MediaWiki\MediaWikiServices;
+use MediaWiki\Linker\LinkRenderer;
 use MediaWiki\Title\Title;
 use Skin;
 
 class Hooks implements
 	\MediaWiki\Skins\Hook\SkinAfterPortletHook
 {
+	private LinkRenderer $linkRenderer;
+
+	public function __construct(
+		LinkRenderer $linkRenderer
+	) {
+		$this->linkRenderer = $linkRenderer;
+	}
+
 	/**
 	 * Render the array as a series of links.
 	 * @param array $tree Categories tree returned by Title::getParentCategoryTree
 	 * @return string Separated by &gt;, terminate with "\n"
 	 */
-	private static function drawCategoryBrowser( array $tree ): string {
+	private function drawCategoryBrowser( array $tree ): string {
 		$return = '';
-		$linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
 		foreach ( $tree as $element => $parent ) {
 			if ( !$parent ) {
 				# element start a new list
 				$return .= "\n";
 			} else {
 				# grab the others elements
-				$return .= self::drawCategoryBrowser( $parent ) . ' &gt; ';
+				$return .= $this->drawCategoryBrowser( $parent ) . ' &gt; ';
 			}
 			# add our current element to the list
 			$eltitle = Title::newFromText( $element );
-			$return .= $linkRenderer->makeLink( $eltitle, $eltitle->getText() );
+			$return .= $this->linkRenderer->makeLink( $eltitle, $eltitle->getText() );
 		}
 		return $return;
 	}
@@ -46,7 +53,7 @@ class Hooks implements
 			$parenttree = $title->getParentCategoryTree();
 			# Skin object passed by reference cause it can not be
 			# accessed under the method subfunction drawCategoryBrowser
-			$tempout = explode( "\n", self::drawCategoryBrowser( $parenttree ) );
+			$tempout = explode( "\n", $this->drawCategoryBrowser( $parenttree ) );
 			# Clean out bogus first entry and sort them
 			unset( $tempout[0] );
 			asort( $tempout );
